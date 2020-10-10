4b10be59054bdda74af1528372d26dcba2d9883e
Jean Bisutti <jean.bisutti@gmail.com>
2020-10-10 20:46:08 +0200

    Add comment element to EnableQueriesWithoutBindParameters annotation

--- sql/sql-annotations/src/main/java/org/quickperf/sql/annotation/EnableQueriesWithoutBindParameters.java
+++ sql/sql-annotations/src/main/java/org/quickperf/sql/annotation/EnableQueriesWithoutBindParameters.java
@@ -22,4 +22,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface EnableQueriesWithoutBindParameters {
+
+    String comment() default "";
+
 }
