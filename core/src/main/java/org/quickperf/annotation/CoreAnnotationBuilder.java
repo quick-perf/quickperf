43ed859214622a9a7d0880caedb93558509e3152
Jean Bisutti <jean.bisutti@gmail.com>
2020-10-23 10:25:26 +0200

    Add Javadoc on CoreAnnotationBuilder methods allowing to build global annotations

--- core/src/main/java/org/quickperf/annotation/CoreAnnotationBuilder.java
+++ core/src/main/java/org/quickperf/annotation/CoreAnnotationBuilder.java
@@ -24,6 +24,9 @@ public class CoreAnnotationBuilder {
 
     private CoreAnnotationBuilder() {}
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.DebugQuickPerf} annotation.
+     */
     public static DebugQuickPerf debugQuickPerf() {
         return new DebugQuickPerf() {
             @Override
@@ -33,6 +36,9 @@ public class CoreAnnotationBuilder {
         };
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.DisableGlobalAnnotations} annotation.
+     */
     public static DisableGlobalAnnotations disableGlobalAnnotations(final String comment) {
         return new DisableGlobalAnnotations() {
             @Override
@@ -46,11 +52,17 @@ public class CoreAnnotationBuilder {
         };
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.DisableGlobalAnnotations} annotation.
+     */
     public static DisableGlobalAnnotations disableGlobalAnnotations() {
         String comment = "";
         return disableGlobalAnnotations(comment);
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.DisableQuickPerf} annotation.
+     */
     public static DisableQuickPerf disableQuickPerf(final String comment) {
         return new DisableQuickPerf() {
             @Override
@@ -64,10 +76,16 @@ public class CoreAnnotationBuilder {
         };
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.DisableQuickPerf} annotation.
+     */
     public static DisableQuickPerf disableQuickPerf() {
         return disableQuickPerf("");
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.FunctionalIteration} annotation.
+     */
     public static FunctionalIteration functionalIteration() {
         return new FunctionalIteration() {
             @Override
@@ -77,6 +95,9 @@ public class CoreAnnotationBuilder {
         };
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.DisplayAppliedAnnotations} annotation.
+     */
     public static DisplayAppliedAnnotations displayAppliedAnnotations() {
         return new DisplayAppliedAnnotations() {
             @Override
@@ -86,6 +107,9 @@ public class CoreAnnotationBuilder {
         };
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.MeasureExecutionTime} annotation.
+     */
     public static MeasureExecutionTime measureExecutionTime() {
         return new MeasureExecutionTime() {
             @Override
@@ -95,22 +119,37 @@ public class CoreAnnotationBuilder {
         };
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.ExpectMaxExecutionTime} annotation.
+     */
     public static ExpectMaxExecutionTime expectMaxExecutionTimeOfHours(final int hours) {
         return expectMaxExecutionTime(hours, 0, 0, 0);
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.ExpectMaxExecutionTime} annotation.
+     */
     public static ExpectMaxExecutionTime expectMaxExecutionTimeOfMinutes(final int minutes) {
         return expectMaxExecutionTime(0, minutes, 0, 0);
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.ExpectMaxExecutionTime} annotation.
+     */
     public static ExpectMaxExecutionTime expectMaxExecutionTimeOfSeconds(final int seconds) {
         return expectMaxExecutionTime(0, 0, seconds, 0);
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.ExpectMaxExecutionTime} annotation.
+     */
     public static ExpectMaxExecutionTime expectMaxExecutionTimeOfMilliSeconds(final int ms) {
         return expectMaxExecutionTime(0, 0, 0, ms);
     }
 
+    /**
+     *Allows to build {@link org.quickperf.annotation.ExpectMaxExecutionTime} annotation.
+     */
     public static ExpectMaxExecutionTime expectMaxExecutionTime( final int hours, final int minutes
                                                                , final int seconds, final int milliSeconds) {
         return new ExpectMaxExecutionTime() {
