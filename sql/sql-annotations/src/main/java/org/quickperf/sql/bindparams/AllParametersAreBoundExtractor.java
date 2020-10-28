469dc0eddd7fb27734df251e8e46ca3d57c287df
Mohamed Karaga <mohamed.karaga@zenika.com>
2020-10-28 20:41:09 +0100

    :white_check_mark: fix and add unit tests for unbindParameters test(DisableQueriesWithoutBindParametersTest) with following sql statements : between, update, insert, like, nested statements

--- sql/sql-annotations/src/main/java/org/quickperf/sql/bindparams/AllParametersAreBoundExtractor.java
+++ sql/sql-annotations/src/main/java/org/quickperf/sql/bindparams/AllParametersAreBoundExtractor.java
@@ -17,17 +17,22 @@ import org.quickperf.measure.BooleanMeasure;
 import org.quickperf.sql.SqlExecution;
 import org.quickperf.sql.SqlExecutions;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AllParametersAreBoundExtractor implements ExtractablePerformanceMeasure<SqlExecutions, BooleanMeasure> {
 
     public static final AllParametersAreBoundExtractor INSTANCE = new AllParametersAreBoundExtractor();
 
-    private AllParametersAreBoundExtractor() {}
+    private AllParametersAreBoundExtractor() {
+    }
 
     @Override
     public BooleanMeasure extractPerfMeasureFrom(SqlExecutions sqlExecutions) {
         for (SqlExecution sqlExecution : sqlExecutions) {
             for (QueryInfo query : sqlExecution.getQueries()) {
-                if(oneUnbindParameter(query)) {
+                if (oneUnbindParameter(query)) {
                     return BooleanMeasure.FALSE;
                 }
             }
@@ -37,17 +42,18 @@ public class AllParametersAreBoundExtractor implements ExtractablePerformanceMea
 
     private boolean oneUnbindParameter(QueryInfo query) {
         final String queryString = query.getQuery();
-
         String queryStrippedOfQuotes = stripQuotesContent(queryString);
-
         final String queryInLowerCase = queryStrippedOfQuotes.toLowerCase();
-
-        if(queryInLowerCase.contains("where")){
+        if (queryInLowerCase.contains("where") || queryInLowerCase.contains("values") || queryInLowerCase.contains("set")) {
             String[] splitWhere = queryInLowerCase.split("where");
-            String[] andOrParts = splitWhere[1].split("and | or");
-            for (String wherePart : andOrParts) {
-                wherePart = wherePart.replaceAll(" ", "");
-                if (!wherePart.contains("=?")) {
+            List<String> andOrParts = new ArrayList<>();
+            for (String s : splitWhere) {
+                String[] andOrPart = s.split(" and | or ");
+                Collections.addAll(andOrParts, andOrPart);
+            }
+            for (String andOrPart : andOrParts) {
+                SqlKeyWord sqlKeyWord = SqlKeyWord.wherePartSplitter(andOrPart);
+                if (sqlKeyWord.hasUnBindParameter(andOrPart)) {
                     return true;
                 }
             }
@@ -69,4 +75,81 @@ public class AllParametersAreBoundExtractor implements ExtractablePerformanceMea
         return queryStrippedOfQuotes;
     }
 
+    private enum SqlKeyWord {
+        IN("in") {
+            @Override
+            boolean hasUnBindParameter(String andOrPart) {
+                if (SqlKeyWord.isReferencedNestedStatement(andOrPart)) {
+                    return false;
+                }
+                String[] inPart = andOrPart.split(SqlKeyWord.IN.getValue());
+                String[] commaParts = inPart[1].split(",");
+                for (String part : commaParts) {
+                    if (!part.contains("?")) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        },
+        VALUES("values") {
+            @Override
+            boolean hasUnBindParameter(String andOrPart) {
+                String[] inPart = andOrPart.split(SqlKeyWord.VALUES.getValue());
+                String[] commaParts = inPart[1].split(",");
+                for (String part : commaParts) {
+                    if (!part.contains("?")) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        },
+        SET("set") {
+            @Override
+            boolean hasUnBindParameter(String andOrPart) {
+                String[] inPart = andOrPart.split(SqlKeyWord.SET.getValue());
+                String[] commaParts = inPart[1].split(",");
+                for (String part : commaParts) {
+                    if (!part.contains("?")) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        },
+        OTHER("other") {
+            @Override
+            boolean hasUnBindParameter(String andOrPart) {
+                andOrPart = andOrPart.replaceAll(" ", "");
+                if (SqlKeyWord.isReferencedNestedStatement(andOrPart)) {
+                    return false;
+                }
+                return !andOrPart.contains("?");
+            }
+        };
+        private final String keyWord;
+
+        SqlKeyWord(String keyWord) {
+            this.keyWord = keyWord;
+        }
+
+        static SqlKeyWord wherePartSplitter(String word) {
+            for (SqlKeyWord sqlKeyWord : SqlKeyWord.values()) {
+                if (word.split(" " + sqlKeyWord.getValue() + " ").length > 1) {
+                    return sqlKeyWord;
+                }
+            }
+            return SqlKeyWord.OTHER;
+        }
+
+        public String getValue() {
+            return keyWord;
+        }
+
+        static boolean isReferencedNestedStatement(String andOrPart){
+            return andOrPart.contains("select") || andOrPart.contains("delete");
+        }
+        abstract boolean hasUnBindParameter(String andOrPart);
+    }
 }
