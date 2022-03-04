990cf290764a86ce303ae7b86bb6cea3e04c4b4d
Jean Bisutti <jean.bisutti@gmail.com>
2022-03-04 21:42:12 +0100

    Fix NPE in QuickPerfSpringRunner (JUnit 4) with Surefire

--- spring/junit4-spring4/src/main/java/org/quickperf/spring/junit4/QuickPerfSpringRunner.java
+++ spring/junit4-spring4/src/main/java/org/quickperf/spring/junit4/QuickPerfSpringRunner.java
@@ -552,6 +552,8 @@ public class QuickPerfSpringRunner extends BlockJUnit4ClassRunner {
             QUICK_PERF_SPRING_RUNNER_FOR_SPECIFIC_JVM.setScheduler(scheduler);
         }  else if (quickPerfFeaturesAreDisabled) {
             springRunner.setScheduler(scheduler);
+        }  else if(springRunnerWithQuickPerfFeatures == null) {
+            super.setScheduler(scheduler);
         } else {
             springRunnerWithQuickPerfFeatures.setScheduler(scheduler);
         }
